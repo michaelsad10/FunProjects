9997c777770c805b230033ed5b9c5455f4602568
Michael <michaelsad10@gmail.com>
2018-01-10 15:56:04 -0600

    Working on it

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+ class Deck
+{
+    private class Card
+    {
+        String suit;
+        int rank;
+        Card(String suit, int rank)
+        {
+            this.suit = suit;
+            this.rank = rank;
+        }
+    }
+    int Deck[];
+    Deck(int size)
+    {
+        Deck = new int[size];
+    }
 
-public class Deck {
+    public void put(String suit, int rank)
+    {
+            
+    }
+}
+
+
+class Test
+{
+    public static void main(String[] args)
+    {
+        int size = 52;
+        int[] Deck;
+        Deck Test = new Deck(size);
+    }
 }
